5fc857011a2a95288fa2b699b6354a8e017ef81a
Sug <sugar77.xe@gmail.com>
2025-08-29 12:31:54 +0900

    修正できた！うれし～

--- keijiban/forms.py
+++ keijiban/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import User, Comment
 
-class RegisterForm(forms.Form):
+class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'age']
 
-class PostForm(forms.Form):
+class PostForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['user', 'text']
